93308ed74f93bca540a771ffb046da9f3665ac6d
Reli Tyrväinen <religiosa@users.noreply.github.com>
2026-02-13 10:25:25 +0200

    Update pomodoro.py

--- pomodoro.py
+++ pomodoro.py
@@ -3,20 +3,16 @@ import machine
 import utime
 from pimoroni import RGBLED, Button
 
-# Led-nauhan pituus
+# How many LEDs?
 NUM_LEDS = 50
 
-# Ajastimen pituudet minuuteissa
+# Timer lengths in minutes
 #WORK_MINS = 25
 #BREAK_MINS = 5
 
-# Ajastimen pituudet testaamista varten
-WORK_SECS = 0.5 * 60
-BREAK_SECS = 0.1 * 60
-
-# Värit (Hue, Saturation, Value)
-# Hue välillä 0.0 - 1.0 (0 = punainen, 0.33 = vihreä, 0.66 = sininen)
-# Saturation ja Value myös välillä 0.0 - 1.0
+# Colors (Hue, Saturation, Value)
+# Hue between 0.0 - 1.0 (0 = green, 0.33 = red, 0.66 = blue) (Green and red are "wrong way round" in my LEDs)
+# Saturation and Value between 0.0 - 1.0
 COLOR_WORK = (0.33, 1.0, 0.8)
 COLOR_BREAK = (0.0, 1.0, 0.8)
 COLOR_FLASH = (0.66, 1.0, 1.0)
@@ -25,11 +21,13 @@ led_strip = plasma.WS2812(NUM_LEDS)
 
 user_sw = Button("USER_SW", repeat_time=0)
 button_a = Button("BUTTON_A", repeat_time=0)
+
 try:
     # Button B is only available on Plasma 2040
     button_b = Button("BUTTON_B", repeat_time=0)
 except ValueError:
     button_b = None
+# LED on the Plasma 2040
 led = RGBLED("LED_R", "LED_G", "LED_B")
     
 state = "idle"
@@ -51,7 +49,7 @@ def flash(h, s, v, times=3, delay=0.1):
 led_strip.start()
 
 clear_strip()
-print("Pomodoro-ajastin valmiina. Paina nappia A aloittaaksesi ajastimen")
+print("Pomodoro-timer ready. Press A to start timer. Press A again to stop. Press B to change mode (green = test, red = real timer).")
 
 while True:
     current_time = utime.ticks_ms()
@@ -60,12 +58,12 @@ while True:
         if state == "work" or state == "break":
             state = "idle"
             clear_strip()
-            print("Ajastin nollattu.")
+            print("Timer cleared.")
             
         elif state == "idle":
             state = "work"
             start_time = current_time
-            print(f"Aloitetaan {WORK_SECS / 60} minuutin työjakso")
+            print(f"Begin work phase of {WORK_SECS / 60} minutes")
         
     if button_b.read():
         # Toggle test mode and real pomodoro timer
@@ -73,10 +71,12 @@ while True:
             WORK_SECS = 0.5 * 60
             BREAK_SECS = 0.1 * 60
             led.set_rgb(0, 255, 0)
+            print(f"{WORK_SECS / 60} minutes work phase, {BREAK_SECS / 60} minutes break phase")
         else:
             WORK_SECS = 25 * 60
             BREAK_SECS = 5 * 60
             led.set_rgb(255, 0, 0)
+            print(f"{WORK_SECS / 60} minutes work phase, {BREAK_SECS / 60} minutes break phase")
             
     if state == "work":
         elapsed_time_secs = utime.ticks_diff(current_time, start_time) / 1000
@@ -91,7 +91,7 @@ while True:
         if elapsed_time_secs >= WORK_SECS:
             state = "break"
             start_time = current_time
-            print(f"Aloitetaan {BREAK_SECS / 60} minuutin tauko")
+            print(f"Begin break phase of {BREAK_SECS / 60} minutes")
             flash(*COLOR_FLASH)
             
     elif state == "break":
@@ -107,7 +107,7 @@ while True:
                 
         if elapsed_time_secs >= BREAK_SECS:
             state = "idle"
-            print("Tauko päättyi. Paina nappia aloittaaksesi uuden jakson.")
+            print("Break finished. Press button A to start a new timer.")
             flash(*COLOR_FLASH)
             clear_strip()
                 
